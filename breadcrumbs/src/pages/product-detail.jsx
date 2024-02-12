bb618381c83bcf28aa089da992f152c8162b10e4
DanielSiilva <daniellsousa0614@gmail.com>
2024-02-12 13:43:29 -0300

    feat: pagina ProductDetail

--- breadcrumbs/src/pages/product-detail.jsx
+++ breadcrumbs/src/pages/product-detail.jsx
@@ -1,3 +1,33 @@
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+
 export function ProductDetail() {
-  return <div> ProductDetail </div>;
+  const { id } = useParams();
+
+  const [products, setProducts] = useState(null);
+
+  console.log("detalhes", products);
+
+  useEffect(() => {
+    fetch(`https://dummyjson.com/products/${id}`)
+      .then((response) => response.json())
+      .then((data) => setProducts(data));
+  }, [id]);
+
+  return (
+    <div>
+      <h1> detalhes do produto {products?.title}</h1>
+
+      {products ? (
+        <div>
+          <img src={products.thumbnail} />
+          <h3>product.title</h3>
+          <h3> ${products.price}</h3>
+          <p>{products.description}</p>
+        </div>
+      ) : (
+        <p>Carregando...</p>
+      )}
+    </div>
+  );
 }
