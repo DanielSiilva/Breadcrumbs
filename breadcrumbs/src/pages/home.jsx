da4a1468ed7a6bb0fb3706a1421bd03b25b98b73
DanielSiilva <daniellsousa0614@gmail.com>
2024-02-12 13:20:28 -0300

    Page home - consumo de api e navegacao

--- breadcrumbs/src/pages/home.jsx
+++ breadcrumbs/src/pages/home.jsx
@@ -1,3 +1,35 @@
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+
 export function Home() {
-  return <div> Home</div>;
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    fetch("https://dummyjson.com/products")
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(products);
+        const sliceProducts = data.products.slice(0, 6);
+        setProducts(sliceProducts);
+      });
+  }, []);
+
+  return (
+    <div>
+      <h2> Home Page</h2>
+      <div>
+        {products?.map((item) => (
+          <div key={item.id}>
+            <Link to={"/products/${item.id"} />
+            <img src={item.thumbnail} alt={item.title} />
+            <h3>{item.title}</h3>
+          </div>
+        ))}
+      </div>
+
+      <Link to="/products">
+        <button> Visualizar dos os produtos</button>
+      </Link>
+    </div>
+  );
 }
