bcd4811b8598c7bc54ddb07b578d7afb579f9d89
DanielSiilva <daniellsousa0614@gmail.com>
2024-02-12 12:56:09 -0300

    feat: configuracao do router

--- breadcrumbs/src/App.jsx
+++ breadcrumbs/src/App.jsx
@@ -1,3 +1,21 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Home } from "./pages/home";
+import { ProductListing } from "./pages/product-listing";
+import { ProductDetail } from "./pages/product-detail";
+
 export function App() {
-  return <p>Hello Word!</p>;
+  return (
+    <Router>
+      <div className="app">
+        <h1> Store</h1>
+        {/* <Breadcrumbs /> */}
+        <hr />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/products" element={<ProductListing />} />
+          <Route path="/products/:id" element={<ProductDetail />} />
+        </Routes>
+      </div>
+    </Router>
+  );
 }
